4738247ec69939edac6bf5b835f98adc19950575
xiaoyou <xiaoyou2333@foxmail.com>
2022-05-25 14:28:48 +0000

    finfish1

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
 # # 删除掉不需要的东西
-# rm -rf _nuxt admin api assets configs plugins static themes upload xblog index.php main
+rm -rf _nuxt admin api assets configs plugins static themes upload xblog index.php main themes_back assets_back configs_back plugins_back
 echo "开始下载项目。。。"
 # # 下载xblog的镜像
 # git clone http://gitea.xiaoyou.com/xiaoyou/xblog.git
@@ -19,33 +19,34 @@ echo "编译项目中。。。"
 # cp -R api/plugins .
 # cp -R api/upload .
 # cp api/main main
+# 修改config的配置文件
+# sed -i "s/localhost/host.docker.internal/g" configs/app.ini
 # 设置权限
-sudo chmod -R 777 ./*
+# sudo chmod -R 777 ./*
 # 修改文件名字，避免映射时文件夹丢失
-rm -rf themes_back assets_back configs_back plugins_back
-cp -R themes themes_back
-cp -R assets assets_back
-cp -R configs configs_back
-cp -R plugins plugins_back
+# cp -R themes themes_back
+# cp -R assets assets_back
+# cp -R configs configs_back
+# cp -R plugins plugins_back
 
-echo "删除旧镜像"
-container=$(sudo docker ps -a | grep xblog | awk '{print $1}')
-images=$(sudo docker images | grep xblog | awk '{print $3}')
-# 判断一下容器是否正在运行
-if [[ -n "$container" ]]; then
-    sudo docker rm -f $container
-fi
-if [[ -n "$images" ]]; then
-    sudo docker rmi -f $images
-fi
-# echo $images
+# echo "删除旧镜像"
+# container=$(sudo docker ps -a | grep xblog | awk '{print $1}')
+# images=$(sudo docker images | grep xblog | awk '{print $3}')
+# # 判断一下容器是否正在运行
+# if [[ -n "$container" ]]; then
+#     sudo docker rm -f $container
+# fi
+# if [[ -n "$images" ]]; then
+#     sudo docker rmi -f $images
+# fi
+# # echo $images
 
-echo "构建镜像中。。。"
-sudo docker build -t xblog .
-# # 运行docker
-sudo docker run -itd --add-host=host.docker.internal:host-gateway \
--v /www/wwwroot/xblog/assets:/var/www/html/assets \
--v /www/wwwroot/xblog/configs:/var/www/html/configs \
--v /www/wwwroot/xblog/plugins:/var/www/html/plugins \
--v /www/wwwroot/xblog/themes:/var/www/html/themes \
--p 2334:2334 -e xblog_host="http://192.168.1.13:2334" --name xblog xblog 
+# echo "构建镜像中。。。"
+# sudo docker build -t xblog .
+# # # 运行docker
+# sudo docker run -itd --add-host=host.docker.internal:host-gateway \
+# -v /www/wwwroot/xblog/assets:/var/www/html/assets \
+# -v /www/wwwroot/xblog/configs:/var/www/html/configs \
+# -v /www/wwwroot/xblog/plugins:/var/www/html/plugins \
+# -v /www/wwwroot/xblog/themes:/var/www/html/themes \
+# -p 2334:2334 -e xblog_host="http://192.168.1.13:2334" --name xblog xblog 
--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 # 文件拷贝
 if [ -z "$(ls -A assets)" ];then
     echo "copy assets"
-    cp -rf asset_back/* assets/
+    cp -rf assets_back/* assets/
 fi
 if [ -z "$(ls -A configs)" ];then
     echo "copy configs"
@@ -15,8 +15,14 @@ if [ -z "$(ls -A themes)" ];then
     echo "copy themes"
     cp -rf themes_back/* themes/
 fi
-echo $xblog_host
 # 修改默认配置文件
-
+phpIndex="index.php"
+config=$(cat $phpIndex|grep "SERVER = \".*\"")
+config=${config: 20: -3}
+# 字符串转义
+config=${config//\//\\\/}
+service=${xblog_host//\//\\\/}
+# 修改配置文件
+sed -i "s/${config}/${service}/g" index.php
 # 后台运行
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
